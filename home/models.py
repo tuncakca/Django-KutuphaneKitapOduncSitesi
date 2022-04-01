09f4ae5770baf2f73a3e9f139c3f90af630e5fb0
Tunc AKCA <tuncakca@istanbul.edu.tr>
2022-04-01 14:08:36 +0300

    dinamik favicon

--- home/models.py
+++ home/models.py
@@ -1,3 +1,35 @@
+from telnetlib import STATUS
 from django.db import models
 
 # Create your models here.
+
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(blank=True, max_length=150)
+    keywords = models.CharField(blank=True, max_length=150)
+    description = models.CharField(blank=True, max_length=150)
+    company = models.CharField(blank=True, max_length=150)
+    address = models.CharField(blank=True, max_length=100)
+    phone = models.CharField(blank=True, max_length=15)
+    email = models.CharField(blank=True, max_length=50)
+    smptpServer = models.CharField(blank=True, max_length=50)
+    smtpEmail = models.CharField(blank=True, max_length=50)
+    smtpPassword = models.CharField(blank=True, max_length=50)
+    smtpPort = models.CharField(blank=True, max_length=50)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    instagram = models.CharField(blank=True,max_length=150)
+    twitter = models.CharField(blank=True,max_length=150)
+    youtube = models.CharField(blank=True,max_length=150)
+    aboutUs = models.TextField(blank=True)
+    contact = models.TextField(blank=True)
+    references = models.TextField(blank=True)
+    status = models.CharField(blank=True,max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from home.models import Setting
+
 # Create your views here.
 
 def index(request):
+    setting = Setting.objects.get(pk=1)
     text = "Merhaba Django"
-    context = {'text': text}
+    context = {'setting': setting}
     return render(request, 'index.html', context)
